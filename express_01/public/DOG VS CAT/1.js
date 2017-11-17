4ece34283731933ab2b961b2912c3bdf6a3ae526
nam <abc@gmail.com>
2017-11-17 23:23:32 +0700

    add

---
+++ express_01/public/DOG VS CAT/1.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded" ,function() {
+	window.addEventListener('scroll',function () {
+		var x = document.querySelector('.tongbar');
+		if (window.pageYOffset > 100) {
+				x.classList.add('hienlen');
+		} else {
+				x.classList.remove('hienlen');
+		}
+	})
+	
+	
+},false)
